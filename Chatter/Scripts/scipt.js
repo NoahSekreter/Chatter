2d39594d0b0c33ed1b2c5397ff70b9061873c76b
NoahSekreter <nsekreter@gmail.com>
2017-05-31 13:21:50 -0400

    Added slam jam

---
+++ Chatter/Scripts/scipt.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+
+        console.log($(window).scrollTop());
+
+        if ($(window).scrollTop() > 320) {
+            $('#navbar').addClass('navbar-fixed');
+        }
+
+        if ($(window).scrollTop() < 321) {
+            $('#navbar').removeClass('navbar-fixed');
+        }
+    });
+});
